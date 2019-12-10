7c25559d9e8c1270d3755274ef52086e15c6b009
Andrei Scutariu <andrei.scutariu@endava.com>
2019-12-10 18:40:14 +0200

    add xml ex

---
+++ Week10/Week09.XML.Serialization/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Week09.XML.Serialization
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using System.Xml;
+    using System.Xml.Serialization;
+
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Publisher Publisher { get; set; }
+    }
+
+    public class Publisher
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var list = new List<Book>
+            {
+                new Book {Id = 1, Name = "Carte 1", Publisher = new Publisher {Id = 1, Name = "Mihai"}},
+                new Book {Id = 2, Name = "Carte 2", Publisher = new Publisher {Id = 2, Name = "Andrei"}}
+            };
+
+            // json
+
+
+            // xml
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Book>));
+
+            // stream de scriere [unde o sa scriem in timp ce serializam]
+            StringWriter stringWriter = new StringWriter(); 
+            StringBuilder sb = new StringBuilder();
+
+            XmlWriter writer1 = XmlWriter.Create(stringWriter); 
+            XmlWriter writer2 = XmlWriter.Create(sb); 
+            XmlWriter writer3 = XmlWriter.Create("file.xml"); 
+
+            xmlSerializer.Serialize(writer3, list);
+
+            //var serializeXml = sb.ToString();
+        }
+    }
+}
---
+++ Week10/Week10.Generics.App01/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Week10.Generics.App01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
